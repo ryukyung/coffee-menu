0fc4590d91d9eed45cf6d04fef2528a54396d0d2
ryukyunghye <ryukyung123@naver.com>
2023-04-14 19:50:56 +0900

    :sparkles: (step1/#3): 에스프레소 메뉴판 메뉴 수정하기

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 const $ = (selector) => document.querySelector(selector);
 function App() {
+  $('#espresso-menu-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('menu-edit-button')) {
+      const $menuName = e.target.closest('li').querySelector('.menu-name');
+      const updatedMenuName = prompt(
+        '메뉴명을 수정하세요',
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
